636199a5fd5a04897ab97ea0ff16dae82383885e
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2017-09-30 21:14:38 -0400

    added documentation

--- file_utils.h
+++ file_utils.h
@@ -5,7 +5,10 @@
 #ifndef REVERSEFILE_FILE_UTILS_H
 #define REVERSEFILE_FILE_UTILS_H
 
+// Reads a .txt file into a buffer and returns size.
 int read_file(char* filename, char **buffer);
+
+// Writes text from provided file backwards and saves as a .txt file.
 int write_file(char* filename, char *buffer, int size);
 
 #endif //REVERSEFILE_FILE_UTILS_H
